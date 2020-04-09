59bd8cf5aff0a503693c561d6599c103f18f3dc5
gality860702 <63327350+gality860702@users.noreply.github.com>
2020-04-09 13:05:48 +0800

    練習場景

---
+++ ClassField/Assets/Scenes/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Hp"), Tooltip("顯示怪物的血量"), Range(50, 500)]
+    public int Hp;
+
+    [Header("Mp"), Tooltip("顯示怪物的魔量與上限"), Range(1, 100)]
+    public int Mp = 100;
+
+    [Header("Speed"), Tooltip("顯示怪物的速度值"), Range(1, 50.5f)]
+    public float Speed = 10.5f;
+
+    [Header("Attack"), Tooltip("顯示怪物的攻擊力"), Range(20, 200)]
+    public int Attack = 50;
+
+    [Header("Defence"), Tooltip("顯示怪物的防禦力"), Range(0, 100)]
+    public int Defence;
+
+    [Header("Weapon"), Tooltip("顯示怪物身上的武器")]
+    public string Weapon;
+
+    [Header("Equip"), Tooltip("顯示怪物身上的裝備")]
+    public string Equip = "皮衣";
+
+    [Header("Key"), Tooltip("顯示怪物身上是否有鑰匙")]
+    public bool key;
+
+    [Header("Chest"), Tooltip("顯示怪物身上是否有寶箱")]
+    public bool chest;
+
+
+}
---
+++ ClassField/Assets/Scenes/Enemymanager.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class Enemymanager : MonoBehaviour
+{
+    [Header("怪物1")]
+    public Enemy monster1;
+    [Header("怪物2")]
+    public Enemy monster2;
+    
+    private void Start()
+    {
+        print(monster1.Mp) ;
+        print(monster2.Equip);
+
+        monster1.Hp = 100;
+        monster2.Hp = 300;
+        monster1.Defence =  50;
+        monster1.Weapon = "木棍";
+        monster2.Weapon = "短刀";
+        monster1.key = true;
+        monster2.chest = true;
+            
+    }
+}
